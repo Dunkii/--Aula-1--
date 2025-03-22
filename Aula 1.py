7d8857e8f63a75a200f0616dbd5e29b523297f4b
Dunkii <95259449+Dunkii@users.noreply.github.com>
2025-03-22 16:32:11 -0400

    Atividades da Aula 1

---
+++ Aula 1.py
@@ -0,0 +1,49 @@
+# 1
+#PNumero = int(input("Insira o primeiro Valor:"))
+#SNumero = int(input("Insira o segundo Valor:"))
+#Soma = PNumero + SNumero
+#print("A soma de ", PNumero, " + ", SNumero, " é igual a ", Soma)
+
+#2
+#x = "Python"
+#y = int(3)
+#print(type(x))
+#print(type(y))
+
+#3
+#Num = int(input("Insira um numero: "))
+#NumAnt = Num - 1
+#NumSuc = Num + 1
+#print("O numero escolhido foi ", Num, ", seu numero antecessor é: ", NumAnt, " e seu numero sucessor é: ", NumSuc)
+
+#4
+#Num = int(input("Insira um numero: "))
+#Double = Num * 2
+#Triple = Num * 3
+#Raiz = Num ** 2
+#print("O numero escolhido foi ", Num, ", seu dobro é: ", Double, " seu triplo é: ", Triple, " e sua raiz quadrada é: ", Raiz)
+
+#5
+#Nota1 = int(input("Insira a primeira nota: "))
+#Nota2 = int(input("Insira a segunda nota: "))
+#Media = (Nota1 + Nota2) / 2
+#print("A média do aluno é: ", Media)
+
+#6
+#Num = int(input("Insira um numero: "))
+#print(f"{Num} x 1 = {Num * 1}")
+#print(f"{Num} x 2 = {Num * 2}")
+#print(f"{Num} x 3 = {Num * 3}")
+#print(f"{Num} x 4 = {Num * 4}")
+#print(f"{Num} x 5 = {Num * 5}")
+#print(f"{Num} x 6 = {Num * 6}")
+#print(f"{Num} x 7 = {Num * 7}")
+#print(f"{Num} x 8 = {Num * 8}")
+#print(f"{Num} x 9 = {Num * 9}")
+#print(f"{Num} x 10 = {Num * 10}")
+
+#7
+#Price = float(input("Insira o valor do produto: "))
+#Desc = Price * 0.05
+#NPrice = Price - Desc
+#print("O valor com desconto é: ", NPrice)
